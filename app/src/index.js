faa70ec6a85f2adbe6d83ee7b1d5198b497a71b8
ClarrieO5 <clarrieohare@gmail.com>
2024-06-06 18:14:59 -0400

    Ads fix

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "node:http";
-import { publicPath } from "./app/static/lib/index.js";
+import { publicPath } from "../static/lib/index.js";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux";
